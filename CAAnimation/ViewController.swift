ba3bb9cabfe68e302c8e8464b14b1e8a8cdc835d
周旗 <1163018319@qq.com>
2020-02-27 14:00:51 +0800

    添加实例视频

--- CAAnimation/ViewController.swift
+++ CAAnimation/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var discView: UIView!
+    @IBOutlet weak var musicImgV: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //music 音符动画
+        discView.curveAnimation(imageName: "icon_home_musicnote1", delay: 0)
+        discView.curveAnimation(imageName: "icon_home_musicnote2", delay: 1)
+        discView.curveAnimation(imageName: "icon_home_musicnote3", delay: 2)
         // Do any additional setup after loading the view.
     }
 
